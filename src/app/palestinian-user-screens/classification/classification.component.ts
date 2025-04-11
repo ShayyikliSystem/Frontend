b53ff18d4e4f6e1367b2212d6d27b6fd88f3937b
nadineabuodeh <202108847@bethlehem.edu>
2025-04-12 01:13:10 +0200

    changes

--- src/app/palestinian-user-screens/classification/classification.component.ts
+++ src/app/palestinian-user-screens/classification/classification.component.ts
@@ -1,70 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AlertComponent } from '../../alert/alert.component';
-import { AuthService } from '../../services/auth.service';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { UserService } from '../../services/user.service';
+import { GeneralInfoPanelComponent } from '../dashboard-screen/general-info-panel/general-info-panel.component';
 
 @Component({
   selector: 'app-classification',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    CommonModule,
-    FormsModule,
-    MatTableModule,
-    MatSortModule,
-    MatPaginatorModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatDatepickerModule,
-    MatExpansionModule,
-  ],
+  imports: [GeneralInfoPanelComponent],
   templateUrl: './classification.component.html',
   styleUrl: './classification.component.scss',
 })
-export class ClassificationComponent {
-  balance: any = 'N/A';
-  returnedChecks: any = 'N/A';
-  issuedChecks: any = 'N/A';
-  classification: any = 'N/A';
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit(): void {
-    this.loadUserData();
-  }
-
-  loadUserData(): void {
-    this.userService.getUserBalance().subscribe({
-      next: (data) => (this.balance = data),
-      error: (err) => console.error('Error fetching balance', err),
-    });
-
-    this.userService.getReturnedChecksCount().subscribe({
-      next: (data) => (this.returnedChecks = data),
-      error: (err) =>
-        console.error('Error fetching returned checks count', err),
-    });
-
-    this.userService.getIssuedChecksCount().subscribe({
-      next: (data) => (this.issuedChecks = data),
-      error: (err) => console.error('Error fetching issued checks count', err),
-    });
-
-    this.userService.getUserClassification().subscribe({
-      next: (data) => (this.classification = data),
-      error: (err) => console.error('Error fetching classification', err),
-    });
-  }
-}
+export class ClassificationComponent {}
--- src/app/palestinian-user-screens/support/support-history-filter/support-history-filter.component.ts
+++ src/app/palestinian-user-screens/support/support-history-filter/support-history-filter.component.ts
@@ -5,6 +5,7 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
@@ -27,6 +28,7 @@ import { MatTableModule } from '@angular/material/table';
     MatDatepickerModule,
     MatExpansionModule,
     MatAutocompleteModule,
+    MatIconModule,
   ],
   templateUrl: './support-history-filter.component.html',
   styleUrl: './support-history-filter.component.scss',
