610151af71b3a81828b92fee47b8860a80c91b98
nadineabuodeh <202108847@bethlehem.edu>
2025-04-18 23:09:44 +0300

    structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,51 @@
 import { HttpClientModule } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { LoadingComponentComponent } from './loading-component/loading-component.component';
 import { AuthService } from './services/auth.service';
+import { interval, Subscription, switchMap } from 'rxjs';
+import { SessionTimeoutComponent } from './session-timeout/session-timeout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule, LoadingComponentComponent],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    LoadingComponentComponent,
+    SessionTimeoutComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Shayyikli-System';
+  showTimeout = false;
+  private tokenCheckSub!: Subscription;
 
   constructor(private authService: AuthService, private router: Router) {}
+
   ngOnInit(): void {
-    this.authService.validateToken().subscribe({
-      next: (res) => {
-        console.log('Token validation successful:', res);
-      },
-      error: (err) => {
-        localStorage.removeItem('authToken');
-        localStorage.removeItem('userRoles');
-        sessionStorage.clear();
-      },
-    });
+    if (
+      localStorage.getItem('authToken') &&
+      localStorage.getItem('userRoles')
+    ) {
+      this.tokenCheckSub = interval(1)
+        .pipe(switchMap(() => this.authService.validateToken()))
+        .subscribe({
+          next: () => {},
+          error: () => {
+            this.tokenCheckSub.unsubscribe();
+            localStorage.removeItem('authToken');
+            localStorage.removeItem('userRoles');
+            sessionStorage.clear();
+            this.showTimeout = true;
+          },
+        });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.tokenCheckSub?.unsubscribe();
   }
 }
--- src/app/palestinian-user-screens/user-layout/user-layout.component.ts
+++ src/app/palestinian-user-screens/user-layout/user-layout.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { DashboardSidebarComponent } from '../dashboard-sidebar/dashboard-sidebar.component';
-
 @Component({
   selector: 'app-user-layout',
   standalone: true,
@@ -12,20 +11,6 @@ import { DashboardSidebarComponent } from '../dashboard-sidebar/dashboard-sideba
 })
 export class UserLayoutComponent {
   constructor(private authService: AuthService, private router: Router) {}
-
-  ngOnInit(): void {
-    this.authService.validateToken().subscribe({
-      next: (res) => {
-        console.log('Token validation successful:', res);
-      },
-      error: (err) => {
-        localStorage.removeItem('authToken');
-        localStorage.removeItem('userRoles');
-        sessionStorage.clear();
-        this.router.navigate(['/login']);
-      },
-    });
-  }
 
   logout(): void {
     this.authService.logout();
